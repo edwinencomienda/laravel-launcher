ca475142caa54c2aea18337eb2c197e9e18c020c
Edwin Encomienda <edwintencomienda@gmail.com>
2025-08-09 07:21:13 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 Route::get('/onboarding', [\App\Http\Controllers\OnboardingController::class, 'index'])->name('onboarding');
@@ -36,36 +34,6 @@ Route::get('/github/setup', [\App\Http\Controllers\GitHubAppController::class, '
 Route::post('/github/webhook', [\App\Http\Controllers\GitHubAppController::class, 'handleWebhook'])
     ->withoutMiddleware([VerifyCsrfToken::class])
     ->name('github.webhook');
-
-// Route::get('/test', function () {
-//     $bash = <<<'BASH'
-//     cd /home/raptor/edwin.portal.raptordeploy.com
-//     npm install
-//     npm run build
-//     BASH;
-//     $output = Process::timeout(600)->run($bash)->throw();
-//     dd($output);
-// });
-
-Route::get('/test', function () {
-    $output = executeWithShellExec('cd /home/raptor/edwin.portal.raptordeploy.com && npm install && npx vite build');
-
-    return $output;
-    // echo '<h3>Current Environment:</h3>';
-    // echo '<pre>';
-    // echo 'PATH: '.shell_exec('echo $PATH')."\n";
-    // echo 'USER: '.shell_exec('whoami')."\n";
-    // echo 'PWD: '.shell_exec('pwd')."\n";
-    // echo 'Node version: '.shell_exec('node --version 2>&1')."\n";
-    // echo 'NPM version: '.shell_exec('npm --version 2>&1')."\n";
-    // echo 'Which node: '.shell_exec('which node 2>&1')."\n";
-    // echo 'Which npm: '.shell_exec('which npm 2>&1')."\n";
-    // echo '</pre>';
-});
-
-// Route::get('/github/repos', fn () => \Illuminate\Support\Facades\Http::withToken(trim(Storage::get('github_access_token.txt')))
-//     ->get('https://api.github.com/installation/repositories')
-//     ->json());
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
