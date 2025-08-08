85173df1b12bef2fbce0d5f9876a0fe255c47d12
Edwin Encomienda <edwintencomienda@gmail.com>
2025-08-09 07:17:33 +0800

    updates

--- app/Jobs/PerformOnboardingJob.php
+++ app/Jobs/PerformOnboardingJob.php
@@ -10,7 +10,6 @@ use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Process;
 use Throwable;
 
@@ -42,21 +41,20 @@ class PerformOnboardingJob implements ShouldQueue
         $adminDomain = Setting::getByKey(SettingsEnum::ADMIN_DOMAIN);
         $siteDomain = Setting::getByKey(SettingsEnum::SITE_DOMAIN);
 
-        DB::transaction(function () use ($onboardingData, $siteDomain, $adminDomain) {
-            // step 1: create user
-            $this->updateOnboardingStatusMessage('Creating user');
-            User::create([
-                'name' => $onboardingData['name'],
-                'email' => $onboardingData['email'],
-                'password' => $onboardingData['password'], // this was already hashed during the onboarding process
-            ]);
+        // step 1: create user
+        $this->updateOnboardingStatusMessage('Creating user');
+        User::create([
+            'name' => $onboardingData['name'],
+            'email' => $onboardingData['email'],
+            'password' => $onboardingData['password'], // this was already hashed during the onboarding process
+        ]);
 
-            // step 2: clone repository
-            $this->updateOnboardingStatusMessage('Cloning repository');
-            $repo = (new GetGithubRepoListAction)->handle()->firstWhere('full_name', $onboardingData['repo_name']);
-            $repoBranch = $onboardingData['repo_branch'] ?? $repo['default_branch'];
-            $githubAccessToken = getGithubAccessToken();
-            $bash = <<<BASH
+        // step 2: clone repository
+        $this->updateOnboardingStatusMessage('Cloning repository');
+        $repo = (new GetGithubRepoListAction)->handle()->firstWhere('full_name', $onboardingData['repo_name']);
+        $repoBranch = $onboardingData['repo_branch'] ?? $repo['default_branch'];
+        $githubAccessToken = getGithubAccessToken();
+        $bash = <<<BASH
             cd /home/raptor
             git clone -b {$repoBranch} https://x-access-token:{$githubAccessToken}@github.com/{$repo['full_name']} /home/raptor/{$siteDomain}
             cd /home/raptor/{$siteDomain}
@@ -75,50 +73,49 @@ class PerformOnboardingJob implements ShouldQueue
             php artisan config:cache
             php artisan migrate
             BASH;
-            Process::timeout(600)->run($bash)->throw();
+        Process::timeout(600)->run($bash)->throw();
 
-            // step 2.1: build assets if found.
-            $packageJsonPath = "/home/raptor/{$siteDomain}/package.json";
-            $packageJson = file_exists($packageJsonPath) ? file_get_contents($packageJsonPath) : null;
-            if ($packageJson) {
-                $packageJson = json_decode($packageJson, true);
-                if (isset($packageJson['scripts']['build'])) {
-                    $this->updateOnboardingStatusMessage('Building site assets');
-                    Process::timeout(600)->run("cd /home/raptor/{$siteDomain} && npm install && npm run build", function ($type, $output) {
-                        echo $output;
-                    })->throw();
-                }
+        // step 2.1: build assets if found.
+        $packageJsonPath = "/home/raptor/{$siteDomain}/package.json";
+        $packageJson = file_exists($packageJsonPath) ? file_get_contents($packageJsonPath) : null;
+        if ($packageJson) {
+            $packageJson = json_decode($packageJson, true);
+            if (isset($packageJson['scripts']['build'])) {
+                $this->updateOnboardingStatusMessage('Building site assets');
+                Process::timeout(600)->run("cd /home/raptor/{$siteDomain} && npm install && npm run build", function ($type, $output) {
+                    echo $output;
+                })->throw();
             }
+        }
 
-            // step 3: create nginx site
-            $this->updateOnboardingStatusMessage('Creating nginx site');
-            $nginxSite = new CreateNginxSiteAction;
-            $nginxSite->handle(
-                rootPath: "/home/raptor/{$siteDomain}/public",
-                domain: $siteDomain,
-            );
-            // this is the admin panel to manage the sites and deployments
-            $this->updateOnboardingStatusMessage('Creating admin site');
-            $nginxSite = new CreateNginxSiteAction;
-            $nginxSite->handle(
-                rootPath: '/home/raptor/raptor/public',
-                domain: $adminDomain,
-            );
+        // step 3: create nginx site
+        $this->updateOnboardingStatusMessage('Creating nginx site');
+        $nginxSite = new CreateNginxSiteAction;
+        $nginxSite->handle(
+            rootPath: "/home/raptor/{$siteDomain}/public",
+            domain: $siteDomain,
+        );
+        // this is the admin panel to manage the sites and deployments
+        $this->updateOnboardingStatusMessage('Creating admin site');
+        $nginxSite = new CreateNginxSiteAction;
+        $nginxSite->handle(
+            rootPath: '/home/raptor/raptor/public',
+            domain: $adminDomain,
+        );
 
-            // step 4: generate domain ssl
-            $this->updateOnboardingStatusMessage('Generating domain ssl');
-            $ssl = new GenerateDomainSslAction;
-            $ssl->handle(
-                domain: $siteDomain,
-            );
-            $ssl->handle(
-                domain: $adminDomain,
-            );
+        // step 4: generate domain ssl
+        $this->updateOnboardingStatusMessage('Generating domain ssl');
+        $ssl = new GenerateDomainSslAction;
+        $ssl->handle(
+            domain: $siteDomain,
+        );
+        $ssl->handle(
+            domain: $adminDomain,
+        );
 
-            // reload nginx
-            $this->updateOnboardingStatusMessage('Reloading nginx');
-            Process::run('sudo nginx -t && sudo nginx -s reload')->throw();
-        });
+        // reload nginx
+        $this->updateOnboardingStatusMessage('Reloading nginx');
+        Process::run('sudo nginx -t && sudo nginx -s reload')->throw();
 
         $this->updateStatus('completed');
     }
