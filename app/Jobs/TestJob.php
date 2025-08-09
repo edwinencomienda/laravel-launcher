215f6a1eb9c2385ed75643d41c0b985008f4e984
Edwin Encomienda <edwintencomienda@gmail.com>
2025-08-09 19:56:01 +0800

    updates

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class TestJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // check if dir exists
+        if (! is_dir('/home/raptor')) {
+            mkdir('/home/raptor', 0755, true);
+        }
+    }
+}
